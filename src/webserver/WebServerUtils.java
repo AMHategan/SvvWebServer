dcc4951d86549aa8767f3a31d5657cef0f3ba500
AMHategan <49283426+AMHategan@users.noreply.github.com>
2020-10-27 20:08:34 +0200

    Added webserver class

---
+++ src/webserver/WebServerUtils.java
@@ -0,0 +1,18 @@
+package webserver;
+
+import java.io.BufferedReader;
+import java.io.OutputStream;
+
+public class WebServerUtils {
+
+	public String getRequest(BufferedReader in) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void sendResponse(OutputStream out) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
