03e17839161f9740b63b453f5f141d5bfc1dcf38
AMHategan <49283426+AMHategan@users.noreply.github.com>
2021-01-08 17:05:12 +0200

    some updates regarding implementation

---
+++ SvvWebServer-master/src/webserver/Response.java
@@ -0,0 +1,15 @@
+package webserver;
+
+public class Response {
+
+	public String getMessage() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public int getContentType() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
